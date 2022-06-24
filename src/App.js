7b2a14591a7d39845c57424edaed30e896a073dc
gimhanul <gimhanuly@gmail.com>
2022-06-24 13:45:11 +0900

    ADD :: Header component (#2)
    - logo png -> svg
    - header component
    - header scss
    - header app.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <>
             <BrowserRouter>
-                {/*<Header/>*/}
+                <Header/>
                 <Routes>
                     {/*<Route path="/" element={<Main/>}/>*/}
                 </Routes>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,51 @@
+import "./Header.scss";
+import {Link} from "react-router-dom";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="header-inner">
+                <div className="header-logo-wrapper">
+                    <Link to="/">
+                        <img src="./images/logo.svg" alt="awoody" width={130}/>
+                    </Link>
+                </div>
+
+                <div className="header-navigation-wrapper">
+                    <ul className="header-navigation">
+                        <li>
+                            <Link to="">
+                                위치
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                일정
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                공지
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+
+                <div className="header-user-field-wrapper">
+                    <ul className="header-user-field">
+                        <li>
+                            <Link to="">
+                                로그인
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to="">
+                                회원가입
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+    )
+}
