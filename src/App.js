900c0009cce8a9898485fd878fb25bf1a291b17d
gimhanul <gimhanuly@gmail.com>
2022-06-24 12:04:11 +0900

    SET :: react-router-dom (#1)
    - router dom 추가
    - app.js router 세팅

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
 function App() {
   return (
-      <></>
+      <>
+        <BrowserRouter>
+          {/*<Header/>*/}
+          <Routes>
+            {/*<Route path="/" element={<Main/>}/>*/}
+          </Routes>
+        </BrowserRouter>
+      </>
   );
 }
 
