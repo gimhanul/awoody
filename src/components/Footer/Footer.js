b6c78536de462894edf8b8988d76b360b8ebc34a
gimhanul <gimhanuly@gmail.com>
2022-06-26 20:48:51 +0900

    ADD :: login 안 되어 있으면 header, footer 안 뜨게(#6)
    - checkObjectIsEmpty 추가
    - 위 함수 이용해서 header, footer 표시

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,27 @@
 import "./Footer.scss";
+import {useContext} from "react";
+import {UserContext} from "../../App";
+import {checkObjectIsEmpty} from "../../utils/checkObjectIsEmpty";
 
 export default function Footer() {
+
+    const {user} = useContext(UserContext);
+
+
     return (
-        <footer>
-            <div className="footer-sns">
-                @gimhanul
-                <a href="https://github.com/gimhanul/awoody" target="_blank"><img src="./images/github.svg" alt="github"/></a>
-                <a href="https://instagram.com/iduzyoy" target="_blank"><img src="./images/instagram.svg" alt="instagram"/></a>
-                <a href="https://velog.io/@gimhanul" target="_blank"><img src="./images/velog.svg" alt="velog"/></a>
-            </div>
-        </footer>
+        <>
+            {!checkObjectIsEmpty(user) &&
+                <footer>
+                    <div className="footer-sns">
+                        @gimhanul
+                        <a href="https://github.com/gimhanul/awoody" target="_blank"><img src="./images/github.svg"
+                                                                                          alt="github"/></a>
+                        <a href="https://instagram.com/iduzyoy" target="_blank"><img src="./images/instagram.svg"
+                                                                                     alt="instagram"/></a>
+                        <a href="https://velog.io/@gimhanul" target="_blank"><img src="./images/velog.svg" alt="velog"/></a>
+                    </div>
+                </footer>
+            }
+        </>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,51 +1,61 @@
 import "./Header.scss";
 import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {UserContext} from "../../App";
+import {checkObjectIsEmpty} from "../../utils/checkObjectIsEmpty";
 
 export default function Header() {
+
+    const {user} = useContext(UserContext);
+
     return (
-        <header>
-            <div className="header-inner">
-                <div className="header-logo-wrapper">
-                    <Link to="/">
-                        <img src="./images/logo.svg" alt="awoody" width={130}/>
-                    </Link>
-                </div>
+        <>
+            {!checkObjectIsEmpty(user) &&
+                <header>
+                    <div className="header-inner">
+                        <div className="header-logo-wrapper">
+                            <Link to="/">
+                                <img src="./images/logo.svg" alt="awoody" width={130}/>
+                            </Link>
+                        </div>
 
-                <div className="header-navigation-wrapper">
-                    <ul className="header-navigation">
-                        <li>
-                            <Link to="/position">
-                                위치
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="">
-                                일정
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="">
-                                공지
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                        <div className="header-navigation-wrapper">
+                            <ul className="header-navigation">
+                                <li>
+                                    <Link to="/position">
+                                        위치
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">
+                                        일정
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">
+                                        공지
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
 
-                <div className="header-user-field-wrapper">
-                    <ul className="header-user-field">
-                        <li>
-                            <Link to="">
-                                로그인
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="">
-                                회원가입
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </header>
+                        <div className="header-user-field-wrapper">
+                            <ul className="header-user-field">
+                                <li>
+                                    <Link to="">
+                                        {user.authority}
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="">
+                                        {user.name}
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </header>
+            }
+        </>
     )
 }
