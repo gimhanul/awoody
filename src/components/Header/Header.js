e42df2994b56bec6e221fb1a08de1f6e636df2d2
gimhanul <gimhanuly@gmail.com>
2022-06-28 10:03:36 +0900

    ADD :: header authority별로 분리 (#7)
    - logo 분리
    - nav 분리
    - authority check function 추가

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 import {useContext} from "react";
 import {UserContext} from "../../App";
 import {checkObjectIsEmpty} from "../../utils/checkObjectIsEmpty";
+import {checkUserAuthority} from "../../utils/checkUserAuthority";
 
 export default function Header() {
 
@@ -14,28 +15,54 @@ export default function Header() {
                 <header>
                     <div className="header-inner">
                         <div className="header-logo-wrapper">
-                            <Link to="/">
-                                <img src="./images/logo.svg" alt="awoody" width={130}/>
-                            </Link>
+                            {checkUserAuthority(user, "학생") &&
+                                <Link to="/">
+                                    <img src="./images/logo.svg" alt="awoody" height={33}/>
+                                </Link>
+                            }
+                            {checkUserAuthority(user, "선생님") &&
+                                <Link to="/teacher">
+                                    <img src="./images/teacherLogo.svg" alt="awoody-teacher" height={33}/>
+                                </Link>
+
+                            }
                         </div>
 
                         <div className="header-navigation-wrapper">
                             <ul className="header-navigation">
-                                <li>
-                                    <Link to="/position">
-                                        위치
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="">
-                                        일정
-                                    </Link>
-                                </li>
-                                <li>
-                                    <Link to="">
-                                        공지
-                                    </Link>
-                                </li>
+                                {checkUserAuthority(user, "학생") &&
+                                    <>
+                                        <li>
+                                            <Link to="/position">
+                                                위치
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="">
+                                                일정
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="">
+                                                공지
+                                            </Link>
+                                        </li>
+                                    </>
+                                }
+                                {checkUserAuthority(user, "선생님") &&
+                                    <>
+                                        <li>
+                                            <Link to="/teacher/position">
+                                                위치
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="">
+                                                공지
+                                            </Link>
+                                        </li>
+                                    </>
+                                }
                             </ul>
                         </div>
 
