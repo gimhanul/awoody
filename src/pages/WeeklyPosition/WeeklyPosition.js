ac05e762369809c777847e37d92c4141390880de
gimhanul <gimhanuly@gmail.com>
2022-08-05 05:01:40 +0900

    ADD :: EditPositionModal (#13)
    - EditPostModal 구현
    - 기능 없음 그냥 겉만 만들었어염

--- src/pages/WeeklyPosition/WeeklyPosition.js
+++ src/pages/WeeklyPosition/WeeklyPosition.js
@@ -7,12 +7,14 @@ import {UserContext} from "../../App";
 import {useNavigate} from "react-router-dom";
 import {checkObjectIsEmpty} from "../../utils/checkObjectIsEmpty";
 import Button from "../../components/Button/Button";
+import EditPositionModal from "../../components/EditPositionModal/EditPositionModal";
 
 export default function WeeklyPosition() {
 
     const [activeIndex, setActiveIndex] = useState(0);
     const [editMode, setEditMode] = useState(false);
     const [weeklyPosition, setWeeklyPosition] = useState(weeklyPositionData.week)
+    const [editPositionModalIsOpen, setEditPositionModalIsOpen] = useState(false);
 
     const {user} = useContext(UserContext);
     const navigation = useNavigate();
@@ -36,53 +38,61 @@ export default function WeeklyPosition() {
     }, [])
 
     return (
-        <section className="purple-background">
-            <div className="weekly-position">
-                <div className="weekly-position-top">
-                    <p className="title">이번 일주일의 내 위치</p>
-                    <ul className="weekly-position--days">
+        <>
+            <section className="purple-background">
+                <div className="weekly-position">
+                    <div className="weekly-position-top">
+                        <p className="title">이번 일주일의 내 위치</p>
+                        <ul className="weekly-position--days">
+                            {
+                                weeklyPositionData.week.map((w, index) => (
+                                    <li className={`weekly-position--days-day ${activeIndex === index ? "active" : ""}`}
+                                        onClick={() => setActiveIndex(index)}>
+                                        {w.day}
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+
+                    <ul className="weekly-position-days-day--position">
                         {
-                            weeklyPositionData.week.map((w, index) => (
-                                <li className={`weekly-position--days-day ${activeIndex === index ? "active" : ""}`}
-                                    onClick={() => setActiveIndex(index)}>
-                                    {w.day}
+                            weeklyPosition[activeIndex].position.map((p, index) => (
+                                <li className="weekly-position-days-day--position-position">
+                                    <Position
+                                        size="big"
+                                        position={p.position}
+                                        classType={p.classType}
+                                        isChecked={p.isChecked}
+                                        toggleCheck={() => toggleCheck(index)}
+                                        editMode={editMode}
+                                        startEditMode={() => {
+                                            setEditMode(true)
+                                            toggleCheck(index)
+                                        }}
+                                    />
                                 </li>
                             ))
                         }
                     </ul>
-                </div>
 
-                <ul className="weekly-position-days-day--position">
-                    {
-                        weeklyPosition[activeIndex].position.map((p, index) => (
-                            <li className="weekly-position-days-day--position-position">
-                                <Position
-                                    size="big"
-                                    position={p.position}
-                                    classType={p.classType}
-                                    isChecked={p.isChecked}
-                                    toggleCheck={() => toggleCheck(index)}
-                                    editMode={editMode}
-                                    startEditMode={() => {
-                                        setEditMode(true)
-                                        toggleCheck(index)
-                                    }}
-                                />
-                            </li>
-                        ))
-                    }
-                </ul>
-
-                <div className="weekly-position-button">
-                    {editMode &&
-                        <Button
-                            text="수정하기"
-                            willDo={() => console.log("수정수정")}
-                        />
-                    }
+                    <div className="weekly-position-button">
+                        {editMode &&
+                            <Button
+                                text="수정하기"
+                                willDo={() => setEditPositionModalIsOpen(true)}
+                            />
+                        }
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+            <EditPositionModal
+                isOpen={editPositionModalIsOpen}
+                closeModal={() => setEditPositionModalIsOpen(false)}
+                weeklyPosition={weeklyPosition}
+                editWeeklyPosition={() => console.log("변경변경 ㅋㅋ")}
+            />
+        </>
     )
 }
 
